17cb50fe5ab0c380ef009cbbacd751021b46b96d
Louie Soulen Kurenai <168345503+ruikurenaii@users.noreply.github.com>
2025-04-29 21:29:53 +0800

    Add v1.2.0 changelog

--- markdown.js
+++ markdown.js
@@ -2,7 +2,17 @@
   markdown content goes here. the changelog changes if they're released.
 */
 
+
 const markdown = `
+## v1.2.0
+\`Codename: Cryoengine\`
+\nAdded new pp system version \`04-29-25\`
+\nThis update adds the angle and star rating rework to the pp system.
+
+- Added more if statements
+- Added star rating and \`starRatingBonus\`
+- Added \`angleBonus\`
+
 ## v1.1.0
 \`Codename: Biocircuit\`
 \nAdded new pp system version \`04-13-25\`.
